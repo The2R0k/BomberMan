6b611ac7bcbffbc0d3cc9e309ffac2c88fc41739
Prichman <prichmen@gmail.com>
2016-08-10 11:07:58 +0700

    Added common.h file

---
+++ src/common.h
@@ -0,0 +1,16 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+enum Cell {
+  EMPTY = 0,
+  PLAYER_1 = 1,
+  PLAYER_2,
+  PLAYER_3,
+  PLAYER_4,
+  BOX,
+  WALL,
+  BOMB,
+  FIRE
+};
+
+#endif /* _COMMON_H_ */
