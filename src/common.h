f61d45b16a591abc1889e60288002ad1a5238cd6
Prichman <prichmen@gmail.com>
2016-08-10 11:58:16 +0700

    Added comments to enum Cell and refactor struct Field.

--- src/common.h
+++ src/common.h
@@ -1,19 +1,20 @@
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
+/*  Enum to represent a cell of game field. */
 enum Cell {
   EMPTY = 0,
   PLAYER_1 = 1,
   PLAYER_2,
   PLAYER_3,
   PLAYER_4,
-  BOX,
-  WALL,
+  BOX,  /*  Can be destroyed. */
+  WALL, /*  Can not be destroyed.*/
   BOMB,
-  FIRE
+  FIRE  /*  Fire effect after detonating. */
 };
 
-struct field{
+struct Field {
   int **location;
   int score;
   int lenght;
