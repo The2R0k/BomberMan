2dd1622089b7d16e28185fad425698563defdf71
AntonBobrovsky <ileon009@gmail.com>
2016-08-10 15:37:49 +0700

    add position.h

---
+++ src/include/position.h
@@ -0,0 +1,15 @@
+/*  Position structure.
+ *  \file position.h
+ */
+
+#ifndef _POSITION_H_
+#define _POSITION_H_
+
+#include <stdint.h>
+
+struct Position {
+  uint8_t x,
+  uint8_t y;
+};
+
+#endif /* _POSITION_H_ */
