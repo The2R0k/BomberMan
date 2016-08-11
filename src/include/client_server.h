fe658d84b7d1204453a3c97bf16088371be3fb1e
The2R0k <2r0k@bk.ru>
2016-08-11 13:54:25 +0700

    Create client_server.h

---
+++ src/include/client_server.h
@@ -0,0 +1,26 @@
+#ifndef _CLIENT_SERVER_H_
+#define _CLIENT_SERVER_H_
+
+/*struct for send udp message to server*/
+/*first time sendig id: 0, bomb: 0;0, move 0;0*/
+/*if server not full, server send new id*/
+/*next time, must sending with that id*/
+
+struct ClientToServer
+{
+  uint8_t id;
+  struct Position bomb;
+  struct Position move;
+}__attribute__((__packed__ ));
+
+/*struct to recv message from server*/
+/*first time sending new id, and new field*/
+
+struct ServerToClient
+{
+  uint8_t id;
+  struct Field field;
+  struct Dklb stats;
+}__attribute__((__packed__ ));
+
+#endif
