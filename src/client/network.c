c0dc12270f3d71ce7abbcb72aea4110f7a7dde89
AntonBobrovsky <ileon009@gmail.com>
2016-08-11 10:21:06 +0700

    some changes

--- src/client/network.c
+++ src/client/network.c
@@ -11,6 +11,17 @@ struct sockaddr_in server_addr;
 void initNetwork() {
   bzero(&server_addr, sizeof(server_addr));
 
+  /*  take ip/port from config file
+   *  format example: 127.0.0.1:4444
+   */
+  FILE *fd;
+  if ((fd = fopen("config", "r")) < 0) {
+    perror("fopen()");
+    exit(-1);
+  }
+
+  fgets(address, sizeof(address), fd);
+
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = inet_addr(address);
   server_addr.sin_port = htons(port);
@@ -39,8 +50,9 @@ void shutdownNetwork() {
   close(udp_sock);
 }
 
-void sendMsg(struct ActionCell msg) {
+void sendMsg(struct ActionCell *msg) {
   memcpy(sendBuf, &msg, ACTIONSIZE);
+  msg->suicide = 0;
 
   if (sendto(udp_sock, sendBuf, ACTIONSIZE, 0, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
     perror("send()");
