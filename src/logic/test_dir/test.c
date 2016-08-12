d2173a3f8f3105f179e36a7085d73ba52d962418
Prichman <prichmen@gmail.com>
2016-08-12 17:56:41 +0700

    Fixed logical errors.
    Fixed PlantBomb, DetonateBomb, DetanateSide, Update interface. Added function for debugging: PrintStatsTable and some in test.c

--- src/logic/test_dir/test.c
+++ src/logic/test_dir/test.c
@@ -7,23 +7,62 @@ struct StatsTable *stats_table;
 
 struct ActionTable action_table;
 
+void MovePlayer(int player_num, struct Position pos)
+{
+  action_table.player_info[player_num].move_pos = pos;
+}
+
+void MakeBomb(int player_num, struct Position pos)
+{
+  action_table.player_info[player_num].bomb_pos = pos;
+}
+
+void MakeMoveAndBomb(int player_num, struct Position move_pos,
+                     struct Position bomb_pos)
+{
+  action_table.player_info[player_num].move_pos = move_pos;
+  action_table.player_info[player_num].bomb_pos = bomb_pos;
+}
+
+void MakeNull(int player_num)
+{
+  action_table.player_info[player_num].move_pos.x = 0;
+  action_table.player_info[player_num].move_pos.y = 0;
+  action_table.player_info[player_num].bomb_pos.x = 0;
+  action_table.player_info[player_num].bomb_pos.y = 0;
+}
+
+void Step(int step)
+{
+  int i;
+
+  for (i = 0; i < step; ++i) {
+    Update(&action_table);
+  }
+}
+
 int main(void)
 {
+  struct Position pos,
+                  pos2;
+
   action_table.player_info[0].suicide = 0;
-  action_table.player_info[0].bomb_pos.x = 0;
-  action_table.player_info[0].bomb_pos.y = 0;
-  action_table.player_info[0].move_pos.x = 1;
-  action_table.player_info[0].move_pos.y = 1;
-
+  MakeNull(0);
+  pos.x = 1;
+  pos.y = 2;
+  MovePlayer(0, pos);
   SetUp(&field, &stats_table);
-  Update(&action_table);
+  Step(1);
 
-  action_table.player_info[0].move_pos.x = 1;
-  action_table.player_info[0].move_pos.y = 1;
-
-  action_table.player_info[0].bomb_pos.x = 1;
-  action_table.player_info[0].bomb_pos.y = 1;
-  Update(&action_table);
-
+  MakeBomb(0, pos);
+  Step(1);
+  pos2 = pos;
+  pos2.y = 1;
+  MakeMoveAndBomb(0, pos2, pos);
+  Step(2);
+  MakeNull(0);
+  Step(2);
+  Step(1);
+  PrintStatsTable();
   return 0;
 }
