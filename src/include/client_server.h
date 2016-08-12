95ac9502f99b6e14c7d8e608183e82fd231992d7
The2R0k <2r0k@bk.ru>
2016-08-12 17:59:58 +0700

    added Doing enum.
     in struct ClientToServer shanged type of doing field

--- src/include/client_server.h
+++ src/include/client_server.h
@@ -9,10 +9,20 @@
 #include position.h
 #include common.h
 
+enum Doing 
+{
+  NOTHING = 0,
+  BOMB = 1,
+  MOVE_LEFT = 2,
+  MOVE_RIGHT,
+  MOVE_TOP,
+  MOVE_DOWN
+};
+
 struct ClientToServer
 {
   uint8_t id;
-  uint8_t doing;
+  enum Doing doing;
 }__attribute__((__packed__ ));
 
 /*struct to recv message from server*/
