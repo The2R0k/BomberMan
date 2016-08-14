651130a3cb626c5413612c2e5e586a998150fea8
Prichman <prichmen@gmail.com>
2016-08-14 17:17:37 +0700

    Fixed code style and compile errors in client_server.h.

--- src/include/client_server.h
+++ src/include/client_server.h
@@ -1,39 +1,51 @@
+/**
+  \file
+  \brief File contains structs to provide client-server interaction.
+*/
+
 #ifndef _CLIENT_SERVER_H_
 #define _CLIENT_SERVER_H_
 
-/*struct for send udp message to server*/
-/*first time sendig id: 0, bomb: 0;0, move 0;0*/
-/*if server not full, server send new id*/
-/*next time, must sending with that id*/
+/* Struct for send udp message to server
+ * first time sendig id: 0, bomb: 0;0, move 0;0
+ * if server not full, server send new id
+ * next time, must sending with that i
+ */
 
-#include position.h
-#include common.h
+#include "position.h"
+#include "common.h"
 
-enum Doing 
-{
+/**
+  \brief Enum to represent user actions.
+*/
+enum Doing {
   NOTHING = 0,
   BOMB = 1,
-  MOVE_LEFT = 2,
+  MOVE_LEFT,
   MOVE_RIGHT,
   MOVE_TOP,
   MOVE_DOWN
 };
 
+/**
+  \brief This struct should be sent to server from client.
+*/
 struct ClientToServer
 {
-  uint8_t id;
   enum Doing doing;
-}__attribute__((__packed__ ));
-
-/*struct to recv message from server*/
-/*first time sending new id, and new field*/
-
-struct ServerToClient
-{
   uint8_t id;
+} __attribute__((packed));
+
+/**
+  \briefStruct to recv message from server.
+
+  First time sending new id, and new field.
+*/
+struct ServerToClient {
   struct Field field;
   struct Dklb stats;
-}__attribute__((__packed__ ));
+  uint8_t id;
+} __attribute__((packed));
 
 
 #endif
