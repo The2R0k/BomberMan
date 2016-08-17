c65de206e58a59e2f81bc08fa4c7f9adb3b9fefa
Prichman <prichmen@gmail.com>
2016-08-17 18:49:20 +0700

    Updated bgraphics.h.
    Removed obsolete textures and added doxygen comments.

--- src/include/bgraphics.h
+++ src/include/bgraphics.h
@@ -1,7 +1,16 @@
+/**
+  \file
+  \brief This file contains interface to interact with graphics.
+*/
+
 #ifndef _BGRAPHICS_H_
 #define _BGRAPHICS_H_
 
 #include <SDL2/SDL.h>
+
+/* graphics constants */
+#define PCSIZE 34 /* bitmap block size */
+#define INTERMEDIATE_STR_SIZE 64 /* Size of ways to omg directory */
 
 struct GraphSources {
   SDL_Window *win; /* Window for graphics */
@@ -10,8 +19,9 @@ struct GraphSources {
   SDL_Texture *wall, *empty, *box, *bomb, *interface, *fire;
   SDL_Texture *players[4], *players_b[4]; /* Players's bitmaps */ 
   SDL_Texture *digits[10]; /* interface symbols */
-  SDL_Texture *player_one, *player_two, *player_three, *player_four, *player_one_b, *player_two_b, *player_three_b, *player_four_b; /* Sources */ 
-  SDL_Texture *menuNG, *menuJN, *menuRS, *menuEX, *menuNGC, *menuJNC, *menuRSC, *menuEXC; /* menu items */
+  /* Sources */ 
+  SDL_Texture *menuNG, *menuJN, *menuRS, *menuEX, *menuNGC, *menuJNC,
+              *menuRSC, *menuEXC; /* menu items */
   SDL_Texture *score, *length, *death, *bombi; /* interface items */
 };
 
@@ -21,8 +31,5 @@ void GraphicsInit(struct GraphSources *); /* Initializatin graphics (window, ren
 void CleanGraph(struct GraphSources *); /* Cleainig up all the graphics */
 void PrintNums(int, int, struct GraphSources *); /* Prints values of DKLB. 1st arg - num to print, second arg - y position; */
 
-/* graphics constants */
-#define PCSIZE 34 /* bitmap block size */
-#define INTERMEDIATE_STR_SIZE 64 /* Size of ways to omg directory */
 
 #endif /* _BGRAPHICS_H_ */
